Fix validation metric calculation

src/01_lstm_hyperparameter_tuning.py
@@ -33,11 +33,11 @@ set_seed(40)
 data = pd.read_csv('../data/processed/df_imputed.csv', index_col=0, parse_dates=['TIMESTAMP'])
 
 # Hyperparameter tuning setup
-batch_sizes_list = [16, 32, 64]
-hidden_dim_list = [32, 64, 128, 256]
+batch_sizes_list = [16, 32, 64, 128]
+hidden_dim_list = [32, 64, 128, 256, 512]
 learning_rates_list = [1e-2, 1e-3, 1e-4, 3e-4, 5e-4, 7e-4, 9e-4]
 dropout_list = [0.1, 0.2, 0.3, 0.4, 0.5]
-num_layers_list = [1, 2, 3, 4]
+num_layers_list = [1, 2, 3, 4, 5]
 
 best_model = None
 best_hyperparameters = None
@@ -76,17 +76,17 @@ for i in tqdm(range(args.num_trials)):
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
         train_dl = DataLoader(train_ds, batch_size = batch_size, shuffle = True)
-        val_dl = DataLoader(val_ds, batch_size = batch_size, shuffle = True)
+        val_dl = DataLoader(val_ds, batch_size = batch_size, shuffle = False)
 
         # Call the train_model function with the current set of hyperparameters
-        val_r2, val_mae, model = train_model(train_dl, val_dl, model, optimizer, writer, args.n_epochs, args.device, args.patience)
+        val_r2, val_rmse, model = train_model(train_dl, val_dl, model, optimizer, writer, args.n_epochs, args.device, args.patience)
         
-        print(f"R2 Score: {val_r2:.4f} | MAE: {val_mae:.4f}")
+        print(f"R2 Score: {val_r2:.4f} | MAE: {val_rmse:.4f}")
 
         # Update best model if current model is better
-        if val_mae < best_validation_score:
-            best_validation_score = val_mae
-            best_hyperparameters = {'batch_size': batch_size, 'hidden_units': hidden_dim, 'learning_rate': lr, 'dropout': dropout, 'num_layers': num_layers, 'validation_mae': val_mae, 'validation_r2': val_r2}
+        if val_rmse < best_validation_score:
+            best_validation_score = val_rmse
+            best_hyperparameters = {'batch_size': batch_size, 'hidden_units': hidden_dim, 'learning_rate': lr, 'dropout': dropout, 'num_layers': num_layers, 'validation_rmse': val_rmse, 'validation_r2': val_r2}
             best_model = model
     except:
         print("An error occurred during training. Skipping this trial.")
@@ -96,11 +96,11 @@ writer.close()
 
 # Get the current date and time for the filename
 current_datetime = datetime.datetime.now().strftime("%d%m%Y_%H%M")
-output_file_name = f"best_model_{current_datetime}"
+output_file_name = f"best_lstm_model_{current_datetime}"
 
 # Save the best model and hyperparameters
 torch.save(best_model.state_dict(), f"../models/{output_file_name}.pt")
 with open(f"../models/{output_file_name}_hyperparameters.txt", 'w') as f:
     f.write(str(best_hyperparameters))
 
-print("Best Model Hyperparameters:", best_hyperparameters)
+print("Best LSTM Model Hyperparameters:", best_hyperparameters)

src/02_dnn_hyperparameter_tuning.py
@@ -71,9 +71,7 @@ for i in tqdm(range(args.num_trials)):
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     train_dl = DataLoader(train_ds, batch_size = batch_size, shuffle = True)
-    val_dl = DataLoader(val_ds, batch_size = batch_size, shuffle = True)
-
-
+    val_dl = DataLoader(val_ds, batch_size = batch_size, shuffle = False)
 
     # Call the train_model function with the current set of hyperparameters
     val_r2, val_rmse, model = train_model(train_dl, val_dl, model, optimizer, writer, args.n_epochs, args.device, args.patience)
@@ -91,11 +89,11 @@ writer.close()
 
 # Get the current date and time for the filename
 current_datetime = datetime.datetime.now().strftime("%d%m%Y_%H%M")
-output_file_name = f"best_model_{current_datetime}"
+output_file_name = f"best_dnn_model_{current_datetime}"
 
 # Save the best model and hyperparameters
 torch.save(best_model.state_dict(), f"../models/{output_file_name}.pt")
 with open(f"../models/{output_file_name}_hyperparameters.txt", 'w') as f:
     f.write(str(best_hyperparameters))
 
-print("Best Model Hyperparameters:", best_hyperparameters)
+print("Best DNN Model Hyperparameters:", best_hyperparameters)

src/02_dnn_train_leave_fold_out.py
@@ -38,10 +38,10 @@ parser.add_argument('-p', '--patience', default=10, type=int,
 parser.add_argument('-b', '--batch_size', default=16, type=int,
                     help='Batch size for training the model')
 
-parser.add_argument('-d', '--hidden_dim', default=128, type=int,
+parser.add_argument('-d', '--hidden_dim', default=256, type=int,
                     help='Hidden dimension of the DNN model')
 
-parser.add_argument('-l', '--learning_rate', default=0.001, type=float,
+parser.add_argument('-l', '--learning_rate', default=0.01, type=float,
                     help='Learning rate for the optimizer')
 
 args = parser.parse_args()
@@ -63,6 +63,28 @@ sites = data.index.unique()
 # Get data dimensions to match DNN model dimensions
 INPUT_FEATURES = data.select_dtypes(include = ['int', 'float']).drop(columns = ['GPP_NT_VUT_REF', 'ai', 'chunk_id']).shape[1]
 
+def generate_filename(base_path, n_epochs, patience, batch_size, learning_rate, hidden_units):
+        # Format learning rate to avoid using '.' in file names
+        lr_formatted = f"{learning_rate:.0e}".replace('-', 'm').replace('.', 'p')
+        
+        # Get current date and time
+        current_datetime = datetime.datetime.now().strftime("%d%m%Y_%H%M")
+        
+        # Generate filename
+        filename = (f"dnn_lfo_alldata_epochs{n_epochs}_patience{patience}"
+                        f"_bs{batch_size}_lr{lr_formatted}_hu{hidden_units}"
+                        f"_{current_datetime}")
+        return f"{base_path}/{filename}"
+
+# Constants
+base_path = "../models"
+
+# Generate filename
+filename = generate_filename(base_path, args.n_epochs, args.patience, args.batch_size, args.learning_rate, args.hidden_dim)
+
+# Process and save predictions dataframe
+df_out = data.loc[:, ['TIMESTAMP','GPP_NT_VUT_REF']].copy()
+
 # Initialise data.frame to store GPP predictions, from the trained DNN model
 y_pred_sites = {}
 
@@ -77,6 +99,9 @@ grouped['ai_bins'] = pd.qcut(grouped['ai'], 2, labels=False).astype(str)
 
 # Combine discretized columns into a single categorical column for stratification
 grouped['combined_target'] = grouped['TA_F_MDS_bins'] + '_' + grouped['ai_bins']
+
+all_r2 = []
+all_rmse = []
 
 kf = StratifiedKFold(n_splits=5, shuffle=True)
 for i, (train_index, test_index) in enumerate(kf.split(grouped.index, grouped['combined_target'])):
@@ -98,7 +123,7 @@ for i, (train_index, test_index) in enumerate(kf.split(grouped.index, grouped['c
     val_ds = gpp_dataset(data_val, train_mean, train_std)
 
     train_dl = DataLoader(train_ds, batch_size = args.batch_size, shuffle = True)
-    val_dl = DataLoader(val_ds, batch_size = args.batch_size, shuffle = True)
+    val_dl = DataLoader(val_ds, batch_size = args.batch_size, shuffle = False)
 
     ## Define the model to be trained
     # Initialise the DNN model, set layer dimensions to match data
@@ -117,8 +142,8 @@ for i, (train_index, test_index) in enumerate(kf.split(grouped.index, grouped['c
 
     # Return best validation R2 score and the center used to normalize training data (repurposed for testing on leaf-out site)
     best_r2, best_rmse, model = train_model(train_dl, val_dl,
-                                                 model, optimizer, writer,
-                                                 args.n_epochs, args.device, args.patience)
+                                                model, optimizer, writer,
+                                                args.n_epochs, args.device, args.patience)
     
     print(f"Validation scores for fold {i+1}: R2 = {best_r2:.4f} | RMSE = {best_rmse:.4f}")
 
@@ -160,29 +185,14 @@ for i, (train_index, test_index) in enumerate(kf.split(grouped.index, grouped['c
         y_pred_sites[s] = y_pred[j]
 
     print(f"Test scores for fold {i+1}: R2 = {r2_test:.4f} | RMSE = {rmse_test:.4f}")
+
+    all_r2.append(r2_test)
+    all_rmse.append(rmse_test)
+
     print("")
 
-def generate_filename(base_path, n_epochs, patience, batch_size, learning_rate, hidden_units):
-    # Format learning rate to avoid using '.' in file names
-    lr_formatted = f"{learning_rate:.0e}".replace('-', 'm').replace('.', 'p')
-    
-    # Get current date and time
-    current_datetime = datetime.datetime.now().strftime("%d%m%Y_%H%M")
-    
-    # Generate the filename
-    filename = (f"dnn_lso_alldata_epochs{n_epochs}_patience{patience}"
-                       f"_bs{batch_size}_lr{lr_formatted}_hu{hidden_units}"
-                       f"_{current_datetime}")
-    return f"{base_path}/{filename}"
+print(f"DNN - Mean R2: {np.mean(all_r2):.4f} | Mean RMSE: {np.mean(all_rmse):.4f}")
 
-# Constants
-base_path = "../models"
-
-# Generate filename
-filename = generate_filename(base_path, args.n_epochs, args.patience, args.batch_size, args.learning_rate, args.hidden_dim)
-
-# Process and save predictions dataframe
-df_out = data.loc[:, ['TIMESTAMP','GPP_NT_VUT_REF']].copy()
 for s in y_pred_sites.keys():
     df_out.loc[[i == s for i in df_out.index], 'gpp_dnn'] = np.asarray(y_pred_sites.get(s))
 